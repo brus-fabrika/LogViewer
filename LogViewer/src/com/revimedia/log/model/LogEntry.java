53ce064dd27f7377299cf9503dbff4fae4a4ccb2
Andrey Brus <brus.andrey@gmail.com>
2015-06-25 23:43:08 +0300

    Added column with line number

--- LogViewer/src/com/revimedia/log/model/LogEntry.java
+++ LogViewer/src/com/revimedia/log/model/LogEntry.java
@@ -1,17 +1,22 @@
 package com.revimedia.log.model;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class LogEntry {
+	private final IntegerProperty mLineNumber = new SimpleIntegerProperty();
 	private final StringProperty mPayload = new SimpleStringProperty();
 	private final StringProperty mTimeStamp = new SimpleStringProperty();
 
 	public LogEntry() {
-		this(null);
+		this(null, 0);
 	}
 
-	public LogEntry(String payload) {
+	public LogEntry(String payload, int lineNumber) {
+		this.mLineNumber.set(lineNumber);
+		
 		String[] ss = payload.split(">", 2);
 		if(ss.length == 2) {
 			this.mTimeStamp.set(ss[0]);
@@ -43,5 +48,17 @@ public class LogEntry {
 
 	public StringProperty timestampProperty() {
 		return mTimeStamp;
+	}
+
+	public IntegerProperty lineNumberProperty() {
+		return mLineNumber;
+	}
+	
+	public Integer getmLineNumber() {
+		return mLineNumber.getValue();
+	}
+	
+	public void setLineNumber(Integer number) {
+		this.mLineNumber.setValue(number);
 	}
 }
