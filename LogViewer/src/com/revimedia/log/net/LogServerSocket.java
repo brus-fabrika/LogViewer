2c035ddff0743de30326db10c3edae2cf753a314
Andrey Brus <brus.andrey@gmail.com>
2015-07-11 12:58:48 +0300

    Server accept method is stopped correctly

--- LogViewer/src/com/revimedia/log/net/LogServerSocket.java
+++ LogViewer/src/com/revimedia/log/net/LogServerSocket.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.LogManager;
@@ -13,36 +14,48 @@ import com.revimedia.log.util.Configuration;
 
 public class LogServerSocket implements Runnable {
 	final private Logger log = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
+	final private int mPortNum = Configuration.getInstance().getPropertyAsInt("port", 4444);
 	
 	private File mLogFile;
 
 	ArrayList<ClientLogPooler> mClients = new ArrayList<>();
+	
+	ServerSocket mServerSocket;
 
 	private boolean isServerActivated = true;
-	
+
 	public LogServerSocket(File logFile){
 		this.mLogFile = logFile;
 	}
 	
 	@Override
 	public void run() {
-		int portNum = Configuration.getInstance().getPropertyAsInt("port", 4444);
-		log.info("Start log server on port: " + portNum);
-		try (ServerSocket serverSocket = new ServerSocket(portNum);) {
-			while(isServerActivated ) {
-				Socket clientSocket = serverSocket.accept();
+		log.info("Start log server on port: " + mPortNum);
+		
+		try {
+			mServerSocket = new ServerSocket(mPortNum);
+			while(isServerActivated) {
+				Socket clientSocket = mServerSocket.accept();
 				log.info("Client socket connected: " + clientSocket.getInetAddress());
 				
 				mClients.add(new ClientLogPooler(clientSocket, mLogFile));
 			}
+		} catch(SocketException e) {
+			log.warning("Server interrupted");
 		} catch (IOException e) {
 			log.severe(Arrays.toString(e.getStackTrace()));
 		}
 	}
 
 	public void stopServer() {
-		isServerActivated = false; // TODO: this will not work as accept is a blocking call.
-									// so we can never ever go out from waiting for socket loop
+		isServerActivated = false;
+		if(mServerSocket != null) {
+			try {
+				mServerSocket.close();
+			} catch(IOException ignore) {
+				log.severe(Arrays.toString(ignore.getStackTrace()));
+			}
+		}
 	}
 	
 }
