8d4ab8064bc8b26268520b575ff9c0b8e9311bf3
Andrey Brus <brus.andrey@gmail.com>
2015-11-12 00:30:19 +0200

    Function to add instance to list view

--- LogViewer/src/com/revimedia/log/view/InstanceViewController.java
+++ LogViewer/src/com/revimedia/log/view/InstanceViewController.java
@@ -1,12 +1,12 @@
 package com.revimedia.log.view;
 
-import com.revimedia.log.model.LxpInstance;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
 import javafx.scene.control.cell.CheckBoxListCell;
+
+import com.revimedia.log.model.LxpInstance;
 
 public class InstanceViewController {
 
@@ -17,25 +17,24 @@ public class InstanceViewController {
 	
 	@FXML
 	private void initialize() {
-
-		instanceList.add(new LxpInstance("HART", false));
-		instanceList.add(new LxpInstance("BASE", true));
-		instanceList.add(new LxpInstance("BETA", true));
-		instanceList.add(new LxpInstance("BETA64", false));
-		instanceList.add(new LxpInstance("BMF", false));
-
 		listView.setCellFactory(CheckBoxListCell.forListView(item -> item.instanceCheckedProperty()));
-		
-		instanceList.forEach(instance -> instance.instanceCheckedProperty().addListener(
-			(observable, wasSelected, isSelected) -> {
-				if (isSelected) {
-					System.out.println("Instance " + instance + " is set to " + isSelected);
-				} else {
-					System.out.println("Instance " + instance + " is set to " + isSelected);
-				}
-		}));
-		
-		listView.getItems().addAll(instanceList);
+		addInstance(new LxpInstance("BETA64", false));
 	}
 
+	public void addInstance(LxpInstance instance) {
+		instance.instanceCheckedProperty().addListener(
+				(observable, wasSelected, isSelected) -> {
+					if (isSelected) {
+						System.out.println("Instance " + instance + " is set to " + isSelected);
+					} else {
+						System.out.println("Instance " + instance + " is set to " + isSelected);
+					}
+			}
+		);
+		
+		instanceList.add(instance);
+		
+		listView.getItems().add(instanceList.get(instanceList.size()-1));
+	}
+	
 }
